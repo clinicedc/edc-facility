baee39c2dfb2e04c1dd3202ea8e6270f529e6e27
erikvw <ew2789@gmail.com>
2024-02-05 22:52:31 -0600

    remove check on model

--- edc_facility/system_checks.py
+++ edc_facility/system_checks.py
@@ -34,12 +34,9 @@ def holiday_path_check(app_configs, **kwargs):
 
 
 def holiday_country_check(app_configs, **kwargs):
-    sys.stdout.write(style.SQL_KEYWORD("holiday_country_check ... \r"))
     errors = []
     holiday_path = Path(settings.HOLIDAY_FILE).expanduser()
-    if not sites.all():
-        errors.append(Error("No sites have been registered", id="edc_facility.005"))
-    else:
+    if sites.all():
         with holiday_path.open(mode="r") as f:
             reader = csv.DictReader(f, fieldnames=["local_date", "label", "country"])
             next(reader, None)
@@ -50,9 +47,8 @@ def holiday_country_check(app_configs, **kwargs):
                             "Holiday file has no records for country! Sites are registered "
                             f"for these countries: `{'`, `'.join(sites.countries)}`. Got "
                             f"`{row['country']}`\n",
-                            id="edc_facility.004",
+                            id="edc_facility.W004",
                         )
                     )
                     break
-    sys.stdout.write(style.SQL_KEYWORD("holiday_country_check ... done.\n"))
     return errors
